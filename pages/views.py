ae066169f47171d4721e64f14318ef72f38f394b
eldorjon88 <eldorjonturamurodov2@gmail.com>
2025-10-30 09:34:17 +0500

    bajarildi

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def root_page(request):
+    return render(request, 'pages/root.html')
+
+def home_page(request):
+    return render(request, 'pages/home.html')
+
+def about_page(request):
+    return render(request, 'pages/about.html')
+
+def projects_page(request):
+    return render(request, 'pages/projects.html')
+
+def contact_page(request):
+    return render(request, 'pages/contact.html')
+
